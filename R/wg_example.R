9336b63b983f1cfa4247870a15283ff7d9530a14
StatisticsOnline <collin.cademartori@gmail.com>
2025-04-30 22:31:37 -0400

    Removed variable storing Stan CSV paths to avoid problems with lazy-loading and staged installation.

--- R/wg_example.R
+++ R/wg_example.R
@@ -50,13 +50,15 @@ run_wg_example <- function(refit = FALSE) {
     load(system.file("data", "wg_gs_fit.RData", package = "sc.interface"))
     load(system.file("data", "wg_bp_fit.RData", package = "sc.interface"))
     load(system.file("data", "wg_stan_fit_meta.RData", package = "sc.interface"))
-    stan_files = c(
-      system.file("extdata", "wg_stan_c1.csv", package = "sc.interface"),
-      system.file("extdata", "wg_stan_c2.csv", package = "sc.interface"),
-      system.file("extdata", "wg_stan_c3.csv", package = "sc.interface"),
-      system.file("extdata", "wg_stan_c4.csv", package = "sc.interface")
+    wg_stan_fit_post <- cmdstanr::as_cmdstan_fit(
+      c(
+        system.file("extdata", "wg_stan_c1.csv", package = "sc.interface"),
+        system.file("extdata", "wg_stan_c2.csv", package = "sc.interface"),
+        system.file("extdata", "wg_stan_c3.csv", package = "sc.interface"),
+        system.file("extdata", "wg_stan_c4.csv", package = "sc.interface")
+      ), 
+      check_diagnostics = FALSE
     )
-    wg_stan_fit_post <- cmdstanr::as_cmdstan_fit(stan_files, check_diagnostics = FALSE)
     wg_stan_fit_gq <- stan_model$generate_quantities(
       wg_stan_fit_post, data = wg_stan_fit_meta$stan_data
     )
